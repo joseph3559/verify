d5689a7aeb00f163e261c6a6fba11e45936f4783
joseph3559 <scottjoe3559@gmail.com>
2023-01-20 18:56:20 +0300

    News Dashboad

---
+++ components/TopCards.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+
+const TopCards = () => {
+  return (
+    <div className='grid lg:grid-cols-5 gap-4 p-4'>
+        <div className='lg:col-span-2 col-span-1 bg-white flex justify-between w-full border p-4 rounded-lg'>
+            <div className='flex flex-col w-full pb-4'>
+                <p className='text-2xl font-bold'>7,846</p>
+                <p className='text-gray-600'>Daily News</p>
+            </div>
+            <p className='bg-green-200 flex justify-center items-center p-2 rounded-lg'>
+                <span className='text-green-700 text-lg'>+18%</span>
+            </p>
+        </div>
+        <div className='lg:col-span-2 col-span-1 bg-white flex justify-between w-full border p-4 rounded-lg'>
+            <div className='flex flex-col w-full pb-4'>
+                <p className='text-2xl font-bold'>437,876</p>
+                <p className='text-gray-600'>Verified News</p>
+            </div>
+            <p className='bg-green-200 flex justify-center items-center p-2 rounded-lg'>
+                <span className='text-green-700 text-lg'>+11%</span>
+            </p>
+        </div>
+        <button className='hover:bg-green-50 p-2 shadow-xl rounded-3xl text-lg font-extrabold hover:text-green-700 text-white bg-green-700'>
+            Report Now
+        </button>
+        {/* <div className='bg-white flex justify-between w-full border p-4 rounded-lg'>
+            <div className='flex flex-col w-full pb-4'>
+                <p className='text-2xl font-bold'>11,437</p>
+                <p className='text-gray-600'>Reporters</p>
+            </div>
+            <p className='bg-green-200 flex justify-center items-center p-2 rounded-lg'>
+                <span className='text-green-700 text-lg'>+17%</span>
+            </p>
+        </div> */}
+    </div>
+  )
+}
+
+export default TopCards
---
+++ data/data.js
@@ -0,0 +1,112 @@
+export const data = [
+  {
+    id: 1,
+    name: {
+      first: 'John',
+      last: 'Smith',
+    },
+    total: 27,
+    status: 'On Hold',
+    method: 'Video Recording',
+    date: '15 Minutes ago',
+  },
+  {
+    id: 2,
+    name: {
+      first: 'Chris',
+      last: 'Adams',
+    },
+    total: 11,
+    status: 'Pending',
+    method: 'Images',
+    date: '23 Minutes ago',
+  },
+  {
+    id: 3,
+    name: {
+      first: 'Sarah',
+      last: 'Smith',
+    },
+    total: 49,
+    status: 'Verified',
+    method: 'Witness',
+    date: '1 Hour ago',
+  },
+  {
+    id: 4,
+    name: {
+      first: 'Joseph',
+      last: 'Choo',
+    },
+    total: 150,
+    status: 'Pending',
+    method: 'Video Recording',
+    date: '1 Hour ago',
+  },
+  {
+    id: 5,
+    name: {
+      first: 'Steve',
+      last: 'Harding',
+    },
+    total: 17,
+    status: 'On Hold',
+    method: 'Images',
+    date: '2 Hour ago',
+  },
+  {
+    id: 6,
+    name: {
+      first: 'Laura',
+      last: 'Croft',
+    },
+    total: 12,
+    status: 'Verified',
+    method: 'Voice Recording',
+    date: '3 Hour ago',
+  },
+  {
+    id: 7,
+    name: {
+      first: 'Michael',
+      last: 'Jones',
+    },
+    total: 89,
+    status: 'Verified',
+    method: 'Video Recording',
+    date: '3 Hour ago',
+  },
+  {
+    id: 8,
+    name: {
+      first: 'James',
+      last: 'Bond',
+    },
+    total: 68,
+    status: 'Verified',
+    method: 'Witness',
+    date: '7 Hour ago',
+  },
+  {
+    id: 9,
+    name: {
+      first: 'Haley',
+      last: 'Whiting',
+    },
+    total: 14,
+    status: 'Verified',
+    method: 'Images',
+    date: '1 Day ago',
+  },
+  {
+    id: 10,
+    name: {
+      first: 'Tim',
+      last: 'Thomas',
+    },
+    total: 21,
+    status: 'Pending',
+    method: 'Video Recording',
+    date: '1 Day ago',
+  },
+];
